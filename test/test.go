f4f3ab125abeaa7e0426424708b793219bff3bd2
kekelberi <alexei.gaponov@gmail.com>
2023-01-03 12:42:36 -0600

    save

--- test/test.go
+++ test/test.go
@@ -1,23 +1,80 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"math/rand"
+	"sync/atomic"
+	"time"
 
 )
 
-func main() {
-	s := "lll"
-
-	log.Println(s)
-
-	newS := &s
-
-	change(newS)
-
-	log.Println(s)
-
+type readOp struct {
+	key  int
+	resp chan int
+}
+type writeOp struct {
+	key  int
+	val  int
+	resp chan bool
 }
 
-func change(str *string) {
-	*str = "LOL"
+func main() {
+
+	var readOps int32
+	var writeOps int32
+
+	reads := make(chan readOp)
+	writes := make(chan writeOp)
+
+	go func() {
+		var state = make(map[int]int)
+		for {
+			select {
+			case read := <-reads:
+				read.resp <- state[read.key]
+			case write := <-writes:
+				state[write.key] = write.val
+				write.resp <- true
+			}
+		}
+	}()
+
+	for r := 0; r < 100; r++ {
+		go func() {
+			for {
+				read := readOp{
+					key:  rand.Intn(5),
+					resp: make(chan int)}
+				reads <- read
+				<-read.resp
+				atomic.AddInt32(&readOps, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	for w := 0; w < 10; w++ {
+		go func() {
+			for {
+				write := writeOp{
+					key:  rand.Intn(5),
+					val:  rand.Intn(100),
+					resp: make(chan bool)}
+				writes <- write
+				<-write.resp
+				atomic.AddInt32(&writeOps, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	time.Sleep(time.Second)
+
+	readOpsFinal := atomic.LoadInt32(&readOps)
+
+	fmt.Println("readOps:", readOpsFinal)
+	
+	writeOpsFinal := atomic.LoadInt32(&writeOps)
+	
+	fmt.Println("writeOps:", writeOpsFinal)
 }
